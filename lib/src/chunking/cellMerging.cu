e91fdab9f909e82b903c2097592e17b6f3a09476
Your Name <you@example.com>
2020-10-16 10:00:32 +0200

    Added timing to all chunking related function; Moved chunking related function to subfolder

--- lib/src/chunking/cellMerging.cu
+++ lib/src/chunking/cellMerging.cu
@@ -1,8 +1,6 @@
 #include "pointcloud.h"
 #include "tools.cuh"
 
-
-
 __global__ void kernelMerging(Chunk *outputGrid, Chunk *inputGrid, uint32_t *counter, uint32_t newCellAmount, uint32_t newGridSize, uint32_t oldGridSize, uint32_t threshold) {
 
     int index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -39,13 +37,13 @@ __global__ void kernelMerging(Chunk *outputGrid, Chunk *inputGrid, uint32_t *cou
             chunk_1_1_1->count;
 
     bool containsFinalizedCells = chunk_0_0_0->isFinished ||
-                                      chunk_0_0_1->isFinished ||
-                                      chunk_0_1_0->isFinished ||
-                                      chunk_0_1_1->isFinished ||
-                                      chunk_1_0_0->isFinished ||
-                                      chunk_1_0_1->isFinished ||
-                                      chunk_1_1_0->isFinished ||
-                                      chunk_1_1_1->isFinished;
+                                  chunk_0_0_1->isFinished ||
+                                  chunk_0_1_0->isFinished ||
+                                  chunk_0_1_1->isFinished ||
+                                  chunk_1_0_0->isFinished ||
+                                  chunk_1_0_1->isFinished ||
+                                  chunk_1_1_0->isFinished ||
+                                  chunk_1_1_1->isFinished;
 
     bool isFinalized = (sum >= threshold) || containsFinalizedCells;
 
@@ -104,62 +102,23 @@ void PointCloud::performCellMerging(uint32_t threshold) {
 
         dim3 grid, block;
         create1DKernel(block, grid, newCellAmount);
+
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+
+        cudaEventRecord(start);
         kernelMerging <<<  grid, block >>> (newChunks->devicePointer(), itsGrid[i]->devicePointer(), itsCounter->devicePointer(), newCellAmount, gridSize>>1, gridSize, threshold);
+        cudaEventRecord(stop);
+
+        cudaEventSynchronize(stop);
+
+        float milliseconds = 0;
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        spdlog::info("'performCellMerging' for a grid size of {} took {:f} [ms]", gridSize, milliseconds);
 
         itsGrid.push_back(move(newChunks));
 
         ++i;
     }
-}
-
-__global__ void kernelDistributing(Chunk *grid, Vector3 *cloud, Vector3 *treeData, uint32_t pointCount, Vector3 posOffset, Vector3 size, Vector3 minimum, uint16_t gridSize) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    if(index >= pointCount) {
-        return;
-    }
-
-    Vector3 point = cloud[index];
-
-    // Copied from OctreeConverter
-    float dGridSize = gridSize;
-    auto X = int32_t((point.x - posOffset.x) / 1);
-    auto Y = int32_t((point.y - posOffset.y) / 1);
-    auto Z = int32_t((point.z - posOffset.z) / 1);
-
-    float ux = (float(X) * 1 + posOffset.x - minimum.x) / size.x;
-    float uy = (float(Y) * 1 + posOffset.y - minimum.y) / size.y;
-    float uz = (float(Z) * 1 + posOffset.z - minimum.z) / size.z;
-
-    uint64_t ix = int64_t( fmin (dGridSize * ux, dGridSize - 1.0f));
-    uint64_t iy = int64_t( fmin (dGridSize * uy, dGridSize - 1.0f));
-    uint64_t iz = int64_t( fmin (dGridSize * uz, dGridSize - 1.0f));
-
-    uint64_t gridIndex = ix + iy * gridSize + iz * gridSize * gridSize;
-
-    Chunk *dst = (grid + gridIndex);
-    bool isFinished = dst->isFinished;
-
-    while(!isFinished) {
-        dst = dst->dst;
-        isFinished = dst->isFinished;
-    }
-
-    uint32_t i = atomicAdd(&(dst->indexCount), 1);
-    treeData[dst->treeIndex + i] = cloud[index];
-}
-
-void PointCloud::distributePoints() {
-
-    dim3 grid, block;
-    create1DKernel(block, grid, itsData->pointCount());
-
-    kernelDistributing <<<  grid, block >>> (
-            itsGrid[0]->devicePointer(),
-            itsData->devicePointer(),
-            itsTreeData->devicePointer(),
-            itsData->pointCount(),
-            itsMetadata.cloudOffset,
-            itsMetadata.boundingBox.size(),
-            itsMetadata.boundingBox.minimum,
-            itsGridSize);
 }
